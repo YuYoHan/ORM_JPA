a4406ccdfb6c369c9713ddc89ff24701ce861bc8
YuYoHan <zxzz8014@naver.com>
2023-04-28 14:57:41 +0900

    [Study] persistence context

---
+++ src/main/java/com/example/jpa/hellojpa/JpaMain.java
@@ -0,0 +1,35 @@
+package com.example.jpa.hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager entityManager = emf.createEntityManager();
+
+        EntityTransaction tx = entityManager.getTransaction();
+        tx.begin();
+
+        try {
+            // 비영속
+            Member member =new Member();
+            member.setId(100L);
+            member.setName("Hello");
+
+            // 영속성
+            entityManager.persist(member);
+
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            entityManager.close();
+        }
+
+        emf.close();
+    }
+}
