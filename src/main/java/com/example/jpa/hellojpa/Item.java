d95b95128e300440a059780ce384dc1c213350b7
YuYoHan <zxzz8014@naver.com>
2023-06-22 19:25:06 +0900

    [Study] 단일 테이블

--- src/main/java/com/example/jpa/hellojpa/Item.java
+++ src/main/java/com/example/jpa/hellojpa/Item.java
@@ -3,8 +3,8 @@ package com.example.jpa.hellojpa;
 import javax.persistence.*;
 
 @Entity
-// 조인 전략
-@Inheritance(strategy = InheritanceType.JOINED)
+// 단일 테이블 전략
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 // 부모 클래스에 선언
 // 하위 클래스를 구분하는 용도의 컬럼
 // DTYPE
