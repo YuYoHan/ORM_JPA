2c2da27c2210de7dc914d124f17fab3cf001a6bd
YuYoHan <zxzz8014@naver.com>
2023-06-23 21:19:51 +0900

    [Study] @MappedSuperclass

--- src/main/java/com/example/jpa/hellojpa/BaseEntity.java
+++ src/main/java/com/example/jpa/hellojpa/BaseEntity.java
@@ -12,9 +12,14 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @ToString
-// 매핑 정보만 맞는 부모클래스
+// 매핑 정보만 가지는 부모클래스
+// 상속 관계 매핑 x
+// 엔티티 x
+// 테이블과 매핑 x
+// 부모 클래스를 상속 받는 자식 클래스에 매핑 정보만 제공
+// 추상 클래스 권장
 @MappedSuperclass
-public class BaseEntity {
+public abstract class BaseEntity {
     @Column(name = "insert_member")
     private String createBy;
     private LocalDateTime createDate;
