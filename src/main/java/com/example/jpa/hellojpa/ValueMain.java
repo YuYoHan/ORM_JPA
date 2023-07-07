80148575629321c08b39307bd6a8a15f9757bee1
YuYoHan <zxzz8014@naver.com>
2023-07-07 16:38:11 +0900

    [Study] 기본타입 공부

--- src/main/java/com/example/jpa/hellojpa/ValueMain.java
+++ src/main/java/com/example/jpa/hellojpa/ValueMain.java
@@ -11,6 +11,9 @@ public class ValueMain {
         System.out.println("a = " + a); // 20
         System.out.println("b = " + b); // 10 → 10인 이유 처음에 a 값을 복사하고 별개의 공간을 가지고 있기 때문에 20이 적용 안됨
 
-
+        Integer c = 10;
+        Integer d = c;
+        System.out.println("c = " + a);
+        System.out.println("d = " + b);
     }
 }
